5339768776e367770e1b3918505eab91aefc60c9
Nguyen The Dat <datnthe172223@fpt.edu.vn>
2024-06-11 06:33:24 +0700

    secn

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,49 @@ import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayOut from "./pages/AppLayOut";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:9000";
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There were an error while fetching cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/product" element={<Product />}></Route>
-          <Route path="/pricing" element={<Pricing />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/app" element={<AppLayOut />}></Route>
-          <Route path="*" element={<PageNotFound />}></Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/app" element={<AppLayOut />}>
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Map.tsx
@@ -0,0 +1,8 @@
+import styles from "./Map.module.css";
+import React from "react";
+
+const Map = () => {
+  return <div className={styles.mapContainer}>Map</div>;
+};
+
+export default Map;
